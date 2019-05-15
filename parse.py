8b29ae8f70345800cde8fb9d266143de1d6cc226
Hanjinsu <jinsu501@naver.com>
2019-05-15 14:20:16 +0900

    all parsed

--- parse.py
+++ parse.py
@@ -10,14 +10,41 @@ while 1 :
 	line = r.readline()
 	if not line : break
 
-	line = line.replace("course","#\ncourse")
-	if not line.find("article") or not line.find(":") :
+	if not line.find("article"):
 		line = line.replace("article:","")
+		line = line + "."
 		arr = kkma.morphs(line)
 		art = ",".join(arr)
 		art = "article :" + art;
 		f.write(art+"\n")
-	else : f.write(line)
+	elif not line.find("course") :
+		line = line.replace("course","#\ncourse");
+		f.write(line)
+	elif not line.find("professor") :
+		f.write(line)
+	elif not line.find("star") :
+		f.write(line)
+	elif not line.find("assignment") :
+		f.write(line)
+	elif not line.find("teamproject") :
+		f.write(line)
+	elif not line.find("percentage") :
+		f.write(line)
+	elif not line.find("attendance") :
+		f.write(line)
+	elif not line.find("testnum") :
+		f.write(line)
+	elif not line.find("semester") :
+		f.write(line)
+	elif not line.find("starnum") :
+		f.write(line)
+	elif not line.find("\n") : 
+		f.write(line)
+	else :
+		line = line.replace("article:","")
+		arr = kkma.morphs(line)
+		art = ",".join(arr)
+		f.write(art+"\n")
 
 r.close()	 
 f.close()
